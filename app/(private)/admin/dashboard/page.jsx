bdb0ea0a74aa1f53a59b3b424ac41557b2fa480b
dalienst <oduordalienst@gmail.com>
2025-06-05 19:57:11 +0300

    add centers

--- app/(private)/admin/dashboard/page.jsx
+++ app/(private)/admin/dashboard/page.jsx
@@ -24,7 +24,50 @@ function AdminDashboard() {
     return <LoadingSpinner />;
   }
 
-  return <div>Welcome {account?.email}</div>;
+  return (
+    <div className="p-4">
+      <section className="mb-3">
+        <h2 className="text-2xl font-bold">Hello {account?.name}</h2>
+      </section>
+
+      <section id="summary" className="mb-3">
+        <h6 className="text-xl font-semibold italic underline mb-2">Summary</h6>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 ">
+          <div className="bg-white p-4 rounded shadow">
+            <h4 className="font-bold">Total Centers</h4>
+            <p>{centers?.length || 0}</p>
+          </div>
+
+          <div className="bg-white p-4 rounded shadow">
+            <h4 className="font-bold">Total Centers</h4>
+            <p>{centers?.length || 0}</p>
+          </div>
+
+          <div className="bg-white p-4 rounded shadow">
+            <h4 className="font-bold">Total Centers</h4>
+            <p>{centers?.length || 0}</p>
+          </div>
+        </div>
+      </section>
+
+      <section id="centers" className="mb-3">
+        <h6 className="text-xl font-semibold italic underline mb-2">Centers</h6>
+        {centers?.length > 0 ? (
+          <div>
+            <ul className="list-disc pl-5">
+              {centers.map((center) => (
+                <li key={center.id} className="mb-2">
+                  {center.name}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ) : (
+          <p>No centers available.</p>
+        )}
+      </section>
+    </div>
+  );
 }
 
 export default AdminDashboard;
