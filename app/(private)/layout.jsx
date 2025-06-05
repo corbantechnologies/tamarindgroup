a58506e23d1b0855d4c338ac531896415d0d11ba
dalienst <oduordalienst@gmail.com>
2025-06-05 18:50:05 +0300

    add navbar

--- app/(private)/layout.jsx
+++ app/(private)/layout.jsx
@@ -1,7 +1,15 @@
+"use client";
+
+import Navbar from "@/components/private/Navbar";
 import React from "react";
 
 function PrivateLayout({ children }) {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <Navbar />
+      {children}
+    </div>
+  );
 }
 
 export default PrivateLayout;
