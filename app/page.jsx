77f31cd8a7e431276684fab1b8f7ad81c268a2ed
dalienst <oduordalienst@gmail.com>
2025-06-09 17:10:48 +0300

    work on hero

--- app/page.jsx
+++ app/page.jsx
@@ -5,12 +5,20 @@ import React from "react";
 function LandingPage() {
   return (
     <>
-    <Navbar />
+      <Navbar />
       <div className="container mx-auto p-4">
         {/* top section */}
-        <div className="bg-white shadow w-full">
-          <Image src="/logo.png" alt="hero" width={100} height={100} />
-        </div>
+        <section className="grid sm:grid-cols-2 gap-4 px-4 py-18 items-center">
+          <div>
+            <h1 className="text-5xl font-bold mb-4">The Tamarind Group</h1>
+            <p className="mb-4">
+              Experience Kenya’s best restaurants with Tamarind Group. Enjoy
+              flame-grilled meats at Carnivore restaurant and top seafood in
+              Tamarind Mombasa. Every meal is a culinary adventure. Craving a
+              lively grill, seafood feast, or refined bar experience?
+            </p>
+          </div>
+        </section>
       </div>
     </>
   );
