e041f04c9f835bbe2a8f3c72079c418d52fea68f
dalienst <oduordalienst@gmail.com>
2025-06-17 12:49:13 +0300

    display details

--- app/directory/[id]/page.jsx
+++ app/directory/[id]/page.jsx
@@ -3,7 +3,11 @@ import { msa_directory } from "@/data/directory-msa";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React, { use } from "react";
-import { FaArrowLeft } from "react-icons/fa";
+import {
+  FaArrowAltCircleLeft,
+  FaArrowCircleLeft,
+  FaArrowLeft,
+} from "react-icons/fa";
 
 function DirectoryDetail({ params }) {
   const { id } = use(params);
@@ -33,18 +37,25 @@ function DirectoryDetail({ params }) {
       </div>
 
       <section
-        className="min-h-[40vh]"
+        className="min-h-[40vh] flex items-center"
         style={{
           backgroundImage: `url(${item.image})`,
           backgroundSize: "cover",
           backgroundPosition: "center",
           backgroundRepeat: "no-repeat",
         }}
-      ></section>
+      >
+        <div className="px-3 text-white">
+          <div className="max-w-2xl my-auto p-4">
+            <h1 className="text-5xl font-bold mb-4">{item.name}</h1>
+            <p className="mb-4">{item.subtitle}</p>
+          </div>
+        </div>
+      </section>
 
       <section>
-        <div className="container mx-auto p-4">
-          <div className="mb-3 px-4 py-6 rounded shadow bg-white border-gray-300">
+        <div>
+          <div className="mb-3 px-4 py-6">
             <div className="mb-3 flex justify-between items-center border-b border-gray-300 pb-3">
               <h6 className="text-xl font-semibold">Description</h6>
             </div>
