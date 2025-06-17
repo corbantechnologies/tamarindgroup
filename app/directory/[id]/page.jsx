fe3a40726f5b3a6107434e12a1c194643fbac74c
dalienst <oduordalienst@gmail.com>
2025-06-17 12:14:29 +0300

    fetch item

--- app/directory/[id]/page.jsx
+++ app/directory/[id]/page.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+"use client";
+import { msa_directory } from "@/data/directory-msa";
+import React, { use } from "react";
 
-function DirectoryDetail() {
-  return (
-    <div>DirectoryDetail</div>
-  )
+function DirectoryDetail({ params }) {
+  const { id } = use(params);
+
+  // Find the item in msa_directory by id
+  let item = null;
+  for (const category of msa_directory) {
+    const foundItem = category.items.find((i) => i.id.toString() === id);
+    if (foundItem) {
+      item = foundItem;
+      break;
+    }
+  }
+
+  console.log(item);
+
+  return <div>DirectoryDetail</div>;
 }
 
-export default DirectoryDetail
+export default DirectoryDetail;
