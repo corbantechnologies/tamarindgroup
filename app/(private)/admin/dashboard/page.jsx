48953ee118050e6841c4e787410284cde5d4600f
Dalienst Owino <oduordalienst@gmail.com>
2025-07-14 22:44:42 +0300

    display events

--- app/(private)/admin/dashboard/page.jsx
+++ app/(private)/admin/dashboard/page.jsx
@@ -104,7 +104,7 @@ function AdminDashboard() {
             <h6 className="text-xl font-semibold">Events</h6>
             <button
               className="secondary-button px-2 py-1 rounded text-center leading-[1.5rem]"
-              onClick={() => setEventModalOpen(true)}
+              // onClick={() => setEventModalOpen(true)}
             >
               Create Event
             </button>
