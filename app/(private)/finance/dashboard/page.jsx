6ef90efb8ec038e85670d75ddfa958631c92cc45
dalienst <oduordalienst@gmail.com>
2025-08-18 20:54:07 +0300

    create finance page

--- app/(private)/finance/dashboard/page.jsx
+++ app/(private)/finance/dashboard/page.jsx
@@ -1,5 +1,8 @@
 "use client";
 
+import ApprovalRequestsTable from "@/components/approvalrequests/ApprovalRequestsTable";
+import LoadingSpinner from "@/components/general/LoadingSpinner";
+import { useFetchAccount, useFetchUsers } from "@/hooks/accounts/actions";
 import { useFetchApprovalRequests } from "@/hooks/approvalrequests/actions";
 import React from "react";
 
@@ -10,10 +13,52 @@ function FinanceDashboard() {
     refetch: refetchApprovalRequests,
   } = useFetchApprovalRequests();
 
-  console.log("Approval Requests:", approvalRequests);
-  
+  const {
+    isLoading: isLoadingAccount,
+    data: account,
+    refetch: refetchAccount,
+  } = useFetchAccount();
 
-  return <div>FinanceDashboard</div>;
+  const {
+    isLoading: isLoadingUsers,
+    data: users,
+    refetch: refetchUsers,
+  } = useFetchUsers();
+
+  console.log("Approval Requests:", approvalRequests);
+
+  if (isLoadingAccount || isLoadingApprovalRequests || isLoadingUsers) {
+    return <LoadingSpinner />;
+  }
+
+  return (
+    <div className="container mx-auto p-4">
+      <section className="mb-3">
+        <h2 className="text-2xl font-bold">Hello {account?.name || "User"}</h2>
+      </section>
+
+      <section className="mb-3">
+        <div className="flex items-center justify-between mb-4">
+          <h3 className="text-xl font-semibold">Your Approval Requests</h3>
+
+          <button
+            onClick={() => setIsModalOpen(true)}
+            className="primary-button px-2 py-1 rounded"
+          >
+            Request
+          </button>
+        </div>
+
+        {approvalRequests?.length > 0 ? (
+          <ApprovalRequestsTable approvalrequests={approvalRequests} />
+        ) : (
+          <div className="p-3 bg-gray-400 italic text-white rounded">
+            No approval requests found.
+          </div>
+        )}
+      </section>
+    </div>
+  );
 }
 
 export default FinanceDashboard;
