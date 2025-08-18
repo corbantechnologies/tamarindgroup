e23774cab45d1ca0549b7d4140317fb7d576b1cc
dalienst <oduordalienst@gmail.com>
2025-08-18 21:34:20 +0300

    add approval steps table

--- app/(private)/finance/dashboard/page.jsx
+++ app/(private)/finance/dashboard/page.jsx
@@ -1,17 +1,16 @@
 "use client";
 
 import ApprovalRequestsTable from "@/components/approvalrequests/ApprovalRequestsTable";
+import ApprovalStepsTable from "@/components/approvalsteps/ApprovalStepsTable";
 import LoadingSpinner from "@/components/general/LoadingSpinner";
+import MakeApprovalRequest from "@/forms/approvalrequests/MakeApprovalRequest";
 import { useFetchAccount, useFetchUsers } from "@/hooks/accounts/actions";
 import { useFetchApprovalRequests } from "@/hooks/approvalrequests/actions";
-import React from "react";
+import { useFetchApprovalSteps } from "@/hooks/approvalsteps/actions";
+import React, { useState } from "react";
 
 function FinanceDashboard() {
-  const {
-    isLoading: isLoadingApprovalRequests,
-    data: approvalRequests,
-    refetch: refetchApprovalRequests,
-  } = useFetchApprovalRequests();
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const {
     isLoading: isLoadingAccount,
@@ -25,9 +24,24 @@ function FinanceDashboard() {
     refetch: refetchUsers,
   } = useFetchUsers();
 
-  console.log("Approval Requests:", approvalRequests);
+  const {
+    isLoading: isLoadingApprovalRequests,
+    data: approvalRequests,
+    refetch: refetchApprovalRequests,
+  } = useFetchApprovalRequests();
 
-  if (isLoadingAccount || isLoadingApprovalRequests || isLoadingUsers) {
+  const {
+    isLoading: isLoadingApprovalSteps,
+    data: approvalSteps,
+    refetch: refetchApprovalSteps,
+  } = useFetchApprovalSteps();
+
+  if (
+    isLoadingAccount ||
+    isLoadingApprovalRequests ||
+    isLoadingUsers ||
+    isLoadingApprovalSteps
+  ) {
     return <LoadingSpinner />;
   }
 
@@ -40,7 +54,6 @@ function FinanceDashboard() {
       <section className="mb-3">
         <div className="flex items-center justify-between mb-4">
           <h3 className="text-xl font-semibold">Your Approval Requests</h3>
-
           <button
             onClick={() => setIsModalOpen(true)}
             className="primary-button px-2 py-1 rounded"
@@ -50,13 +63,47 @@ function FinanceDashboard() {
         </div>
 
         {approvalRequests?.length > 0 ? (
-          <ApprovalRequestsTable approvalrequests={approvalRequests} />
+          <ApprovalRequestsTable approvalrequests={approvalRequests.results} />
         ) : (
           <div className="p-3 bg-gray-400 italic text-white rounded">
             No approval requests found.
           </div>
         )}
       </section>
+
+      <section className="mb-3">
+        <div className="flex items-center justify-between mb-4">
+          <h3 className="text-xl font-semibold">
+            Approval Steps Requiring Your Approval
+          </h3>
+        </div>
+
+        {approvalSteps?.length > 0 ? (
+          <ApprovalStepsTable approvalSteps={approvalSteps} />
+        ) : (
+          <div className="p-3 bg-gray-400 italic text-white rounded">
+            No approval steps requiring your approval.
+          </div>
+        )}
+      </section>
+
+      {isModalOpen && (
+        <div className="fixed inset-0 bg-gray-900 bg-opacity-50 flex items-center justify-center z-50">
+          <div className="p-6 rounded-lg shadow-lg w-full max-w-md max-h-full overflow-y-auto">
+            <button
+              className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+              onClick={() => setIsModalOpen(false)}
+            >
+              ✕
+            </button>
+            <MakeApprovalRequest
+              refetch={refetchApprovalRequests}
+              closeModal={() => setIsModalOpen(false)}
+              users={users}
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
