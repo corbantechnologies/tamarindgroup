5fcac1330301a5a2ed781ba3709731380cf83e9f
dalienst <oduordalienst@gmail.com>
2025-08-18 19:43:18 +0300

    work on manager side

--- app/(private)/finance/dashboard/page.jsx
+++ app/(private)/finance/dashboard/page.jsx
@@ -1,8 +1,18 @@
 "use client";
 
+import { useFetchApprovalRequests } from "@/hooks/approvalrequests/actions";
 import React from "react";
 
 function FinanceDashboard() {
+  const {
+    isLoading: isLoadingApprovalRequests,
+    data: approvalRequests,
+    refetch: refetchApprovalRequests,
+  } = useFetchApprovalRequests();
+
+  console.log("Approval Requests:", approvalRequests);
+  
+
   return <div>FinanceDashboard</div>;
 }
 
