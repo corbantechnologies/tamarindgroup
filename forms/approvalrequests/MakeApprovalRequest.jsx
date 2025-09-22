f34a1a6c5f66cc46c83bc37e82836169f538d8e0
dalienst <oduordalienst@gmail.com>
2025-09-22 09:22:39 +0300

    update approval request

--- forms/approvalrequests/MakeApprovalRequest.jsx
+++ forms/approvalrequests/MakeApprovalRequest.jsx
@@ -103,6 +103,7 @@ function MakeApprovalRequest({ refetch, closeModal, users }) {
                 className="mt-1 block w-full border-gray-600 border rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 py-2 px-3"
                 placeholder="Enter request description"
                 rows="4"
+                required
               />
             </div>
 
